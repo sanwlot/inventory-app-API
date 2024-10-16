4a855f865f25a741ab421cad35dab896661c9c6b
sunnySavlot <sunny.savlot@gmail.com>
2024-10-16 15:55:40 +0530

    category_id column added

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -6,24 +6,26 @@ module.exports = {
     res.json(products.rows)
   },
   addProduct: async (req, res) => {
-    const { name, quantity, price, category } = req.body
+    const { name, quantity, price, categoryId } = req.body
+
     await pool.query(
-      "INSERT INTO products (name, quantity, price, category) VALUES ($1, $2, $3, $4)",
-      [name, quantity, price, category]
+      "INSERT INTO products (name, quantity, price, category_id) VALUES ($1, $2, $3, $4)",
+      [name, quantity, price, categoryId]
     )
     const { rows } = await pool.query(
       "SELECT * FROM products WHERE name=$1 LIMIT 1",
       [name]
     )
+    console.log("res: ", rows[0])
     res.json(rows[0])
   },
   updateProduct: async (req, res) => {
-    const { name, quantity, price, category } = req.body
+    const { name, quantity, price, category_id } = req.body
     await pool.query(
-      "UPDATE products SET name= $1, quantity=$2, price=$3, category=$4 WHERE id = $5;",
-      [name, quantity, price, category, req.params.id]
+      "UPDATE products SET name= $1, quantity=$2, price=$3, category_id=$4 WHERE id = $5;",
+      [name, quantity, price, category_id, req.params.id]
     )
-    res.json({ name, quantity, price, category, id: req.params.id })
+    res.json({ name, quantity, price, category_id, id: req.params.id })
   },
   deleteProduct: async (req, res) => {
     const { id } = req.params
